85417ccca6f225a0574cc29ebcc4d90b22b7336a
Endi Caushi <mrendi29@gmail.com>
2020-05-07 02:05:39 -0400

    Finished signup form. Added testing and flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      # Handle successful save
+      flash[:success] = 'Welcome to the Sample App!'
+      redirect_to user_url(@user)
     else
       render 'new'
     end
