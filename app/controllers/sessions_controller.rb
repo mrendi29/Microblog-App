eda962d1ed059b4e896808db98de4d55bce387da
Endi Caushi <mrendi29@gmail.com>
2020-05-10 02:04:42 -0400

    Added login form and tests

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      # log the user and redirect to the user's show page
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy; end
+end
