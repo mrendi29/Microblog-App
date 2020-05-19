2f51240ef34df651d6c9be0759213156983ef7b4
Endi Caushi <mrendi29@gmail.com>
2020-05-19 18:24:29 -0400

    Added Pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: %i[edit update]
+  before_action :logged_in_user, only: %i[index edit update]
   before_action :correct_user, only: %i[edit update]
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
   end
 
   def new
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -10,6 +10,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', help_path
     assert_select 'a[href=?]', about_path
     assert_select 'a[href=?]', contact_path
+    assert_select 'a[href=?]', login_path
 
     get contact_path
     assert_select 'title', full_title('Contact')
@@ -17,5 +18,13 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_template 'users/new'
     assert_select 'title', full_title('Sign up')
+
+    user = users(:michael)
+    log_in_as(user)
+    get root_path
+    assert_select 'a[href=?]', logout_path
+    assert_select 'a[href=?]', users_path
+    assert_select 'a[href=?]', user_path(user)
+    assert_select 'a[href=?]', edit_user_path(user)
   end
 end
