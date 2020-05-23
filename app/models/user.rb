162f0778595e915888ee500245746a5f2c17afce
Endi Caushi <mrendi29@gmail.com>
2020-05-23 00:02:17 -0400

    Initial account activation and mailer template

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { maximum: 255 },
@@ -39,5 +40,19 @@ class User < ApplicationRecord
     return false if remember_digest.nil?
 
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  private
+
+  # Creates and assigns the activation token and digest.
+  def create_activation_digest
+    # create token and digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
+
+  # Converts email to all lower-case
+  def downcase_email
+    email.downcase!
   end
 end
