79d577454b68f2551afe5d6a1993a5446d1f9dee
Endi Caushi <mrendi29@gmail.com>
2020-05-27 22:58:19 -0400

    trying sendspark

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,14 +70,25 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'young-tor-93348.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
+  # sendgrid
+  # ActionMailer::Base.smtp_settings = {
+  #   address: 'smtp.sendgrid.net',
+  #   port: '587',
+  #   authentication: :plain,
+  #   user_name: ENV['SENDGRID_USERNAME'],
+  #   password: ENV['SENDGRID_PASSWORD'],
+  #   domain: 'heroku.com',
+  #   enable_starttls_auto: true
+  # }
+
   ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: '587',
-    authentication: :plain,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
-    enable_starttls_auto: true
+    user_name: 'SMTP_Injection',
+    password: Const::API_KEY,
+    address: 'smtp.sparkpostmail.com',
+    port: 587,
+    enable_starttls_auto: true,
+    format: :html,
+    from: 'mailer@yourawesomeapp.com'
   }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
