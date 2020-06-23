89130b449513c67c616035db7ed41bcbb013516f
Endi Caushi <mrendi29@gmail.com>
2020-06-23 00:25:04 -0400

    web interface for following users- stats and follow form

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,6 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Users
 # Create a main sample user.
 User.create!(name: 'Endi Caushi',
              email: 'mrendi29@gmail.com',
@@ -34,3 +28,11 @@ users = User.order(:created_at).take(6)
   content = Faker::Lorem.sentence(word_count: 5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# Create following relationships.
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
